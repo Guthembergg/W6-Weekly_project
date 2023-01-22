b32a56c60e12b79ac62c99db1fd121ff643dd773
Karim Guettech <karimguettech@gmail.com>
2023-01-22 12:46:32 +0100

    added some comments

--- Assets/script/media.js
+++ Assets/script/media.js
@@ -3,6 +3,6 @@ function myTimer() {
   let root = document.querySelector(":root");
   root.style.setProperty("--Mycolor", `${picker.value}`);
   console.log(picker.value);
-}
+} // questa funzione legge il colore del picker sul sito e lo attribuisce alla variabile css
 
-setInterval(myTimer, 1000);
+setInterval(myTimer, 100); // la funzione è in attesa per un cambio di colore ogni 100 ms
