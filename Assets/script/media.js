84316a64b301183705a27648eee81526e7e8abb0
Karim Guettech <karimguettech@gmail.com>
2023-01-22 14:36:40 +0100

    better js

--- Assets/script/media.js
+++ Assets/script/media.js
@@ -2,7 +2,10 @@ function myTimer() {
   let picker = document.getElementById("colorpicker");
   let root = document.querySelector(":root");
   root.style.setProperty("--Mycolor", `${picker.value}`);
-  console.log(picker.value);
+  //console.log(picker.value);
+  if (picker.value == "#ffffff") {
+    document.getElementById("scritta").style.color = "black"; //cambia il colore della scritta per diventare leggibile nel caso in cui il colore è bianco
+  }
 } // questa funzione legge il colore del picker sul sito e lo attribuisce alla variabile css
 
 setInterval(myTimer, 100); // la funzione è in attesa per un cambio di colore ogni 100 ms
